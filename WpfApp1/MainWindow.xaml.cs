c9c0b66be7badf2876d5f789c1224a1fdf35e296
Mai Yer Lee <mylee@datasci.com>
2019-05-31 10:53:21 -0500

    show example of how to access resources from code-behind, each resource from a different scope (app.xaml, window, controlDockPanel

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -25,5 +25,12 @@ namespace WpfApp1
             InitializeComponent(); // combine XAML and MainWindow at runtime
         }
 
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
+            lbResult.Items.Add(this.FindResource("strWindow").ToString());
+            lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+        }
+
     }
 }
